0d0eda3df913a5d19d264ee7024c02bf2a649ebf
Artiom Mezheynikov <artiom.mezheynikov.work@gmail.com>
2024-10-27 16:25:53 +0300

    fixing bugs in js for dark button

--- js/main.js
+++ js/main.js
@@ -4,31 +4,31 @@ const darkModeBtn = document.querySelector('.dark-mode-btn');
 
 // 1. Проверка тёмной на уровне системных настроек пользователя
 if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-	darkModeBtn.classList.toggle('dark-mode-btn--active');
-	document.body.classList.toggle('dark');
+	darkModeBtn.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
 }
 
 // 2. Проверка тёмной темы в local storage
 if (localStorage.getItem('darkMode') === 'dark') {
-	darkModeBtn.classList.toggle('dark-mode-btn--active');
-	document.body.classList.toggle('dark');
-} else if (localStorage.getItem('darkMode') === 'light') {
-	darkModeBtn.classList.remove('dark-mode-btn--active');
-	document.body.classList.remove('dark');
+	darkModeBtn.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+	darkModeBtn.classList.remove("dark-mode-btn--active");
+	document.body.classList.remove("dark");
 }
 
 // Если меняются системные настройки пользователя , меняем тему
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
-	const newColorShame = e.matches ? 'dark' : 'light';
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+	const newColorShame = event.matches ? "dark" : "light";
 
-	if (newColorShame === 'dark') {
-		darkModeBtn.classList.toggle('dark-mode-btn--active');
-		document.body.classList.toggle('dark');
-		localStorage.setItem('darkMode', 'dark');
+	if (newColorShame === "dark") {
+		darkModeBtn.classList.add("dark-mode-btn--active");
+		document.body.classList.add("dark");
+		localStorage.setItem("darkMode", "dark");
 	} else {
-		darkModeBtn.classList.remove('dark-mode-btn--active');
-		document.body.classList.remove('dark');
-		localStorage.setItem('darkMode', 'light');
+		darkModeBtn.classList.remove("dark-mode-btn--active");
+		document.body.classList.remove("dark");
+		localStorage.setItem("darkMode", "light");
 	}
 });
 
